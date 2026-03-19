87fce2d167bfb7a01f4c7c54008bc46cd992c4e2
Souksawath <souksawath@gmail.com>
2026-03-19 14:09:33 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDlL1a-LZ0gPVGXzD15wMEVS_P9YhZYmGM",
+  authDomain: "ay-e-doc.firebaseapp.com",
+  projectId: "ay-e-doc",
+  storageBucket: "ay-e-doc.firebasestorage.app",
+  messagingSenderId: "467271065376",
+  appId: "1:467271065376:web:4aebb90c6e443394a40145",
+  measurementId: "G-L6DNCEGGJ3"
+};
